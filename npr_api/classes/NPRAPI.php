993d165e498d0e23b77bf502c690c80372526dfb
CrookedNumber <david@crookednumber.com>
2012-03-23 17:34:04 -0400

    Initial commit of npr_push module. Includes major changes to classes in npr_api module.

--- npr_api/classes/NPRAPI.php
+++ npr_api/classes/NPRAPI.php
@@ -33,6 +33,10 @@ class NPRAPI {
 
   }
 
+  function create_NPRML() {
+
+  }
+
   function parse() {
     if (!empty($this->xml)) {
       $xml = $this->xml;
@@ -83,45 +87,6 @@ class NPRAPI {
       $NPRMLElement->value = (string)$element;
     }
     return $NPRMLElement;
-  }
-
-  function create_NPRML($node) {
-    $language = $node->language;
-    $root = new SimpleXMLElement('<nprml></nprml>', 0, FALSE, 'xmlns:nprml=http://api.npr.org/nprml', TRUE);
-    $root->addAttribute('version', '0.92.2');
-    $list = $root->addChild('list');
-    $story = $list->addChild('story');
-
-    if ($node->type == 'blog') {
-      $story->addAttribute('type', 'blog');
-    }
-
-    // FIX
-    $story->addChild('title', substr(htmlentities($node->title), 0, 100));
-
-    if (isset($node->body)) {
-      $story->addChild('text', htmlentities($node->body));
-    }
-
-    if (isset($node->summary)) {
-      $story->addChild('teaser', htmlentities($node->summary));
-    }
-    $story->addChild('storyDate', format_date($node->created, 'custom', "D, d M Y G:i:s O "));
-    $story->addChild('pubDate', $now);
-
-    //if the nprID field is set, (probably because this is an update) send that along too
-    if (isset($node->npr_id)) {
-      $story->addAttribute('id', $node->npr_id);
-    }
-
-    $landing_page = $story->addChild('link', $node->story_url);
-    $landing_page->addAttribute('type', 'html');
-
-    //add the station's org ID
-    $org = $story->addChild('organization');
-    $org->addAttribute('orgId', $node->org_id);
-
-    return $root;
   }
 
   function send_NPRML($xml, $path) {
--- npr_api/classes/NPRAPIDrupal.php
+++ npr_api/classes/NPRAPIDrupal.php
@@ -2,8 +2,11 @@
 
 class NPRAPIDrupal extends NPRAPI {
 
-  function __construct() {
-
+  function __construct($node = NULL) {
+    parent::__construct();
+    if ($node) {
+      $this->node = $node;
+    }
   }
 
   function request($params = array(), $method = 'GET', $data = NULL, $path = 'query', $base = 'http://api.npr.org') {
@@ -38,4 +41,58 @@ class NPRAPIDrupal extends NPRAPI {
 
   }
 
+  function create_NPRML() {
+  
+    $node = $this->node;
+    $language = $node->language;
+    $root = new SimpleXMLElement('<nprml></nprml>', 0, FALSE, 'xmlns:nprml=http://api.npr.org/nprml', TRUE);
+    $root->addAttribute('version', '0.92.2');
+    $list = $root->addChild('list');
+    $story = $list->addChild('story');
+
+    // FIX
+    $story->addChild('title', substr(htmlentities($node->title), 0, 100));
+
+    if (!empty($node->body[$language][0]['value'])) {
+      $story->addChild('text', htmlentities($node->body[$language][0]['value']));
+    }
+
+    if (!empty($node->body[$language][0]['value'])) {
+      $story->addChild('teaser', htmlentities($node->body[$language][0]['value']));
+    }
+    $now = format_date($node->created, 'custom', "D, d M Y G:i:s O ");
+    
+    $story->addChild('storyDate', $now);
+    $story->addChild('pubDate', $now);
+
+    //if the nprID field is set, (probably because this is an update) send that along too
+    if (isset($node->npr_id)) {
+      $story->addAttribute('id', $node->npr_id);
+    }
+
+    //$landing_page = $story->addChild('link', $node->story_url);
+    //$landing_page->addAttribute('type', 'html');
+
+    //add the station's org ID
+    $org = $story->addChild('organization');
+    $org->addAttribute('orgId', variable_get('npr_push_org_id'));
+
+    $this->NPRML = $root->asXML();
+  }
+
+  function push_NPRML() {
+    $org_id = variable_get('npr_push_org_id');
+    $api_key = variable_get('npr_api_station_key');
+    $params = array(
+      'orgId' => $org_id,
+      'apiKey' => $api_key, 
+    );
+    $method = 'PUT';
+    $base = variable_get('npr_push_api_url');
+    $path = 'story';
+    $this->create_NPRML();
+    $data = $this->NPRML;
+    
+    $this->request($params, $method, $data, $path, $base);
+  }
 }
