d819cac1f090e8b908fade2d09460b7cdc10b82b
Matthew Tift <matthew.tift@lullabot.com>
2020-02-10 20:58:52 -0600

    Clean up the NprApiHelpForm class code

--- npr_api/src/Form/NprApiHelpForm.php
+++ npr_api/src/Form/NprApiHelpForm.php
@@ -1,16 +1,14 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\npr_api\Form\NprApiHelpForm.
- */
-
 namespace Drupal\npr_api\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
+use Drupal\Core\Render\RendererInterface;
 
+/**
+ * Provides a help form for API definitions.
+ */
 class NprApiHelpForm extends FormBase {
 
   /**
@@ -20,26 +18,30 @@ class NprApiHelpForm extends FormBase {
     return 'npr_api_help_form';
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, RendererInterface $render) {
+
     $npr_fields = npr_api_get_nprml_fields();
     $header = [
-      t('NPRML Field'),
-      t('Description'),
-      t('Type'),
-      t('Accepted Types for mapping'),
+      $this->t('NPRML Field'),
+      $this->t('Description'),
+      $this->t('Type'),
+      $this->t('Accepted Types for mapping'),
     ];
 
     $form['intro'] = [
-      '#markup' => 'Listed below are the avaiable fields from NPR to map content to, and descriptions. If you are having trouble, please visit the Support Center at <a href="https://nprsupport.desk.com/" target="_blank">https://nprsupport.desk.com/</a>'
-      ];
+      '#markup' => 'Listed below are the avaiable fields from NPR to map content to, and descriptions. If you are having trouble, please visit the Support Center at <a href="https://nprsupport.desk.com/" target="_blank">https://nprsupport.desk.com/</a>',
+    ];
 
     $rows = [];
     $table = NULL;
     foreach ($npr_fields as $name => $field) {
       $new_row = [];
       $accepted_types = "";
-      foreach ($field['accepted_types'] as $type => $value) {
-        $accepted_types .= $value ;
+      foreach ($field['accepted_types'] as $value) {
+        $accepted_types .= $value;
       }
       $description = isset($field['description']) ? $field['description'] : '';
       $new_row = [$name, $description, $field['type'], $accepted_types];
@@ -51,7 +53,7 @@ class NprApiHelpForm extends FormBase {
       '#header' => $header,
       '#rows' => $rows,
     ];
-    $markup = drupal_render($table);
+    $markup = $render($table);
 
     $form['NPRML_fields'] = [
       '#type' => 'fieldset',
@@ -60,8 +62,8 @@ class NprApiHelpForm extends FormBase {
       '#collapsed' => FALSE,
     ];
     $form['NPRML_fields']['info'] = [
-      '#markup' => 'Title, teaser, and body will automatically be mapped to the corresponding NPRML elements.'
-      ];
+      '#markup' => $this->t('Title, teaser, and body will automatically be mapped to the corresponding NPRML elements.')
+    ];
     $form['NPRML_fields']['table'] = ['#markup' => $markup];
 
     return $form;
