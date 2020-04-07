dc834fd02c927d87fe7137ca8cc615045ccf942d
Matthew Tift <matthew.tift@lullabot.com>
2020-04-07 12:36:32 -0500

    Issue #3125741 by mtift: Use options rather than parameters in drush commands

--- npr_pull/src/Commands/NprPullCommands.php
+++ npr_pull/src/Commands/NprPullCommands.php
@@ -32,89 +32,107 @@ class NprPullCommands extends DrushCommands {
    *
    * @param int $story_id
    *   The NPR API id of the story.
-   * @param bool $published
-   *   Should the story be published or not.
-   * @param bool $display_messages
-   *   Messages should be displayed or not.
+   * @param array $options
+   *   Associative array of options.
+   *
+   * @usage drush npr-gs 12345678 --published=FALSE
+   *   Import the story without publishing it.
+   * @usage drush npr-gs 12345678 --display_messages=FALSE
+   *   Import the story, but just log the results and don't display the output.
    *
    * @command npr_pull:getStory
    * @aliases npr-gs
    */
-  public function getStoryById($story_id, $published = TRUE, $display_messages = TRUE) {
+  public function getStoryById($story_id, array $options = ['published' => TRUE, 'display_messages' => TRUE]) {
 
     $params['id'] = $story_id;
     $stories = $this->client->getStories($params);
 
     // Create the stories in Drupal.
     foreach ($stories as $story) {
-      $this->client->addOrUpdateNode($story, $published, $display_messages);
+      $this->client->addOrUpdateNode(
+        $story,
+        $options['published'],
+        $options['display_messages'],
+      );
     }
   }
 
   /**
    * Command to pull NPR stories by topic ID.
    *
-   * @param int $num_results
-   *   The number of results to get from the API.
-   * @param int $topic_id
-   *   The NPR topic ID corrected with topics such as News, Education, or Music.
-   * @param int $start_num
-   *   The page to start from.
-   * @param string $sort
-   *   The storting order, either dateDesc or dateAsc.
-   * @param string $start_date
-   *   The starting date for the request, such as 2020-02-22.
-   * @param string $end_date
-   *   The ending date for the request, such as 2020-02-24.
+   * @param array $options
+   *   Associative array of options.
+   *
+   * @usage drush npr-gst --num_results=50
+   *   Import 50 stories (the maximum for one command) rather than 1.
+   * @usage drush npr-gst --topic_id=1013
+   *   Import 1 education (1013) story.
+   * @usage drush npr-gst --start_num=50 --num_results=20
+   *   Import 20 stories, starting with number 50.
+   * @usage drush npr-gst --sort=dateAsc
+   *   Import the oldest story available.
+   * @usage drush npr-gst --start_date=2020-04-06 --end_date=2020-04-07
+   *   Import all stories (up to 50) published on April 6 or April 7.
    *
    * @command npr_pull:getStoriesByTopicId
    * @aliases npr-gst
    */
-  public function getStoriesByTopicId($num_results = 1, $topic_id = 1001, $start_num = 0, $sort = 'dateDesc', $start_date = '', $end_date = '') {
+  public function getStoriesByTopicId(array $options = [
+    'num_results' => 1,
+    'topic_id' => 1001,
+    'start_num' => 0,
+    'sort' => 'dateDesc',
+    'start_date' => '',
+    'end_date' => '',
+  ]) {
 
-    if ($num_results > 50) {
+    if ($options['num_results'] > 50) {
       throw new \Exception(dt('Because this command accepts a date range, and due to the way the NPR API works, this command cannot process more than 50 stories at one time.'));
     }
 
     $params = [
-      'numResults' => $num_results,
-      'id' => $topic_id,
-      'sort' => $sort,
+      'numResults' => $options['num_results'],
+      'id' => $options['topic_id'],
+      'sort' => $options['sort'],
       'fields' => 'all',
     ];
 
-    if ($start_num > 0) {
-      $params['startNum'] = $start_num;
+    if ($options['start_num'] > 0) {
+      $params['startNum'] = $options['start_num'];
     }
 
-    if (!empty($start_date)) {
-      $params['startDate'] = $start_date;
+    if (!empty($options['start_date'])) {
+      $params['startDate'] = $options['start_date'];
     }
-    if (!empty($end_date)) {
-      $params['endDate'] = $end_date;
-      $start_date = date("Y-m-d");
+    if (!empty($options['end_date'])) {
+      $params['endDate'] = $options['end_date'];
     }
 
     if ($stories = $this->client->getStories($params)) {
       $this->processStories($stories);
     };
-
+    $this->output()->writeln(dt('Process the stories with `drush queue-run npr_api.queue.story` or run cron.'));
   }
 
   /**
-   * Command to pull NPR stories by organization ID.
+   * Command to add NPR stories to the queue by organization ID.
    *
    * @param int $org_id
-   *   The number of results to get from the API.
-   * @param int $num_results
-   *   The number of results to get from the API.
-   * @param int $start_num
-   *   The page to start from.
+   *   The organization ID.
+   * @param array $options
+   *   Associative array of options.
    *
-   * @command npr_pull:getStoriesByOrgId
-   * @aliases npr-gso
+   * @usage drush npr-gst 1 --num_results=5
+   *   Import the 5 most recent stories from National Public Radio.
+   * @usage drush npr-gst 449 --num_results=1000 --start_num=500
+   *   Add 1000 stories from Georgia Public Broadcasting to the queue, starting
+   *   with the 500th result.
+   *
+   * @command npr_pull:getStoriesByTopicId
+   * @aliases npr-gst
    */
-  public function getStoriesByOrgId($org_id = NULL, $num_results = 1, $start_num = 0) {
+  public function getStoriesByOrgId($org_id, array $options = ['num_results' => 1, 'start_num' => 0]) {
 
     if (is_null($org_id)) {
       $this->logger()->info(dt('An organization ID is required.'));
@@ -127,12 +145,12 @@ class NprPullCommands extends DrushCommands {
     ];
 
     // The maximum number of stories per NRP API request is 50.
-    if ($num_results <= 50) {
-      $params['numResults'] = $num_results;
+    if ($options['num_results'] <= 50) {
+      $params['numResults'] = $options['num_results'];
       $stories = $this->client->getStories($params);
     }
-    elseif ($num_results > 50) {
-      for ($i = $start_num; $i < ($num_results + $start_num); $i += 50) {
+    elseif ($options['num_results'] > 50) {
+      for ($i = $options['start_num']; $i < ($options['num_results'] + $options['start_num']); $i += 50) {
         $params['numResults'] = 50;
         $params['startNum'] = $i;
         // Clear the stories.
@@ -141,6 +159,7 @@ class NprPullCommands extends DrushCommands {
         $this->processStories($stories);
       }
     }
+    $this->output()->writeln(dt('Process the stories with `drush queue-run npr_api.queue.story` or run cron.'));
   }
 
   /**
@@ -160,7 +179,7 @@ class NprPullCommands extends DrushCommands {
     $total = $count_after - $count_before;
 
     if ($count_after > $count_before) {
-      $this->output()->writeln(dt('@total stories have been added to the queue (@count_after total). Process the stories with `drush queue-run npr_api.queue.story` or run cron', [
+      $this->output()->writeln(dt('@total stories have been added to the queue (@count_after total).', [
         '@total' => $total,
         '@count_after' => $count_after,
       ]));
