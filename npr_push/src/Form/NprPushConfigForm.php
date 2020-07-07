77176c8825836bfde3a37ed5089edf37ed80fed2
Matthew Tift <matthew.tift@lullabot.com>
2020-07-07 14:52:27 -0500

    Issue #3157551: Add ingest URL to the push settings

--- npr_push/src/Form/NprPushConfigForm.php
+++ npr_push/src/Form/NprPushConfigForm.php
@@ -33,9 +33,15 @@ class NprPushConfigForm extends ConfigFormBase {
 
     $form['org_id'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('NPR Push URL'),
+      '#title' => $this->t('Organization ID'),
       '#default_value' => $config->get('org_id'),
-      '#description' => $this->t('The ID of the organization used for the orgId field when pushing stories to NPR.'),
+      '#description' => $this->t('The ID of the organization (orgId) to use when pushing stories to NPR.'),
+    ];
+    $form['ingest_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Ingest URL'),
+      '#default_value' => $config->get('ingest_url'),
+      '#description' => $this->t('The URL to use when pushing stories to NPR.'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -49,6 +55,7 @@ class NprPushConfigForm extends ConfigFormBase {
     $config = $this->config('npr_push.settings');
 
     $config->set('org_id', $values['org_id']);
+    $config->set('ingest_url', $values['ingest_url']);
     $config->save();
 
     parent::submitForm($form, $form_state);
