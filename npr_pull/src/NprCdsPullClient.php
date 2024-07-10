52a5ac5227f73783eb6f3b27edaeb188d8c68591
Luis Aveiga <luis.aveiga@gmail.com>
2024-07-10 13:49:49 -0500

    improve image file name extraction by decoding image uri first [skip ci]

--- npr_pull/src/NprCdsPullClient.php
+++ npr_pull/src/NprCdsPullClient.php
@@ -1190,7 +1190,7 @@ class NprCdsPullClient implements NprPullClientInterface {
           return;
         }
         // Get the filename.
-        $filename = basename($image_url);
+        $filename = basename(urldecode($image_url));
 
         $directory_uri = 'public://npr_story_images/';
         if (preg_match("/[0-9]{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])/", $image_url)) {
