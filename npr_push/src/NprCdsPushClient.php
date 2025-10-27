38b50a4d91537e01c3d2907e00a46d91edbfdb44
Luis Aveiga <luis.aveiga@gmail.com>
2025-10-27 09:14:59 -0500

    Adding image-wide to the first image

--- npr_push/src/NprCdsPushClient.php
+++ npr_push/src/NprCdsPushClient.php
@@ -446,6 +446,10 @@ class NprCdsPushClient implements NprPushClientInterface {
 
             }
           }
+
+          # Add image-wide only to the first image.
+          $story['images'][0]['rels'][] = 'image-wide';
+
         }
       }
     }
